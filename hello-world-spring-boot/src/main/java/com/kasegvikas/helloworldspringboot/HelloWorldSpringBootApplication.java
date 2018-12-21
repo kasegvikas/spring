7aa2c16d473b3a3b19bbd77484fee9b30015c550
vkasegaonkar <vikas.kasegaonkar@gmail.com>
2018-12-21 10:59:41 +0530

    Sample spring boot application with hello world example

---
+++ hello-world-spring-boot/src/main/java/com/kasegvikas/helloworldspringboot/HelloWorldSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.kasegvikas.helloworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ hello-world-spring-boot/src/test/java/com/kasegvikas/helloworldspringboot/HelloWorldSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kasegvikas.helloworldspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
