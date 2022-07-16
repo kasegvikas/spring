c9f201cff29f97d8bcf523e559da3e2090a5af84
vikas Kasegaonkar <vvk.kasegaonkar@gmail.com>
2022-07-17 00:27:43 +0530

    Removed sysout

--- code-challenge/src/main/java/com/kasegvikas/challenge/codechallenge/Application.java
+++ code-challenge/src/main/java/com/kasegvikas/challenge/codechallenge/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("Hello SpringBootApplication.");
 		SpringApplication.run(Application.class, args);
 	}
 }
