55ca704583679ad28dae7b34d96887891f05819f
pruthvi <pruth@LAPTOP-57K5OL7V>
2022-07-17 00:18:01 +0530

    Test

--- code-challenge/src/main/java/com/kasegvikas/challenge/codechallenge/Application.java
+++ code-challenge/src/main/java/com/kasegvikas/challenge/codechallenge/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+		System.out.println("Hello SpringBootApplication.");
 		SpringApplication.run(Application.class, args);
 	}
 }
